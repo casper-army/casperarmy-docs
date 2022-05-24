173a2364972608c14836042bbcc04c73f7903285
CasperArmy <99138733+casperarmy@users.noreply.github.com>
2022-05-24 18:23:22 +0200

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Documentation',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/casperarmy_logo.png').default,
     description: (
       <>        
         <a href="https://docs2.casperarmy.org/docs/intro">Detailed information about how CasperArmy and its products work.</a>
