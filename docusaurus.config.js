1b024d119b02c4d80b3e3e53f52cc1cee6ce669a
CasperArmy <99138733+casperarmy@users.noreply.github.com>
2022-05-24 21:47:33 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/casperarmy_favicon_logo_white.ico',
   organizationName: 'casperarmy', // Usually your GitHub org/user name.
   projectName: 'casperarmy-docs', // Usually your repo name.
 
