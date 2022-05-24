f99103e98b739bfa4642e47586792b81c8a7acc6
CasperArmy <99138733+casperarmy@users.noreply.github.com>
2022-05-24 23:42:13 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,8 +29,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Welcone on ${siteConfig.title} official documentation`}
+      description="First Venture Capital with DAO on Casper Network">
       <main>
         <HomepageFeatures />
       </main>
